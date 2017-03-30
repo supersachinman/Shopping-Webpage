7ac68961b9630bba9fd5a0b36c63caa444ef9c56
supersachinman <sachinaggarwal96@gmail.com>
2017-03-30 23:49:20 +0530

    Add files via upload

--- shopping.js
+++ shopping.js
@@ -1,13 +1,3 @@
-$('ul.nav li.dropdown').hover(function() {
- 	$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
-	}, function() {
-	$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
-});
-
-
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
 var type_choco = document.getElementById("variantchoco");
 var number_choco = document.getElementById("quantchoco");
 
@@ -166,6 +156,8 @@ kpbutton.addEventListener("click", function(){
 
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
 
 
 
