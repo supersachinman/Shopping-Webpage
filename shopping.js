903b38e17ad984444615e95f97756e9a34049fa4
supersachinman <sachinaggarwal96@gmail.com>
2017-03-30 21:58:29 +0530

    Update shopping.js

--- shopping.js
+++ shopping.js
@@ -1,3 +1,13 @@
+$('ul.nav li.dropdown').hover(function() {
+ 	$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+	}, function() {
+	$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
+
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
 var type_choco = document.getElementById("variantchoco");
 var number_choco = document.getElementById("quantchoco");
 
